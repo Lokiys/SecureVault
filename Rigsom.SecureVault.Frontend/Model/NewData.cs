fd310c99b31bc6f91ae6357ded2c57cd866bbc2c
Paonessa Claudio <Claudio.Paonessa@swissgrid.ch>
2015-07-02 09:30:33 +0200

    Added NewDataModel

---
+++ Rigsom.SecureVault.Frontend/Model/NewData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace Rigsom.SecureVault.Frontend.Model
+{
+    /// <summary>
+    /// TODO: Comment
+    /// </summary>
+    class NewData
+    {
+        /// <summary>
+        /// TODO: Comment
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// TODO: Comment
+        /// </summary>
+        public SecureString SecureData { get; set; }
+
+        /// <summary>
+        /// TODO: Comment
+        /// </summary>
+        public SecureString Password { get; set; }
+
+        /// <summary>
+        /// TODO: Comment
+        /// </summary>
+        /// <param name="password"></param>
+        public NewData(SecureString password)
+        {
+            this.Password = password;
+        }
+    }
+}
