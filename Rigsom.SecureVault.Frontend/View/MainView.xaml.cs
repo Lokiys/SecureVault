ca5c37f98758a85119968ccbcdc507ca0a220c1a
Paonessa Claudio <Claudio.Paonessa@swissgrid.ch>
2015-07-07 07:55:25 +0200

    Added animation

--- Rigsom.SecureVault.Frontend/View/MainView.xaml.cs
+++ Rigsom.SecureVault.Frontend/View/MainView.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,8 +14,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Rigsom.SecureVault.Frontend.View
 {
@@ -53,6 +57,36 @@ namespace Rigsom.SecureVault.Frontend.View
 
                 this.MainFrame.Navigate(passwordPage);
             }
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation transAnimation = new DoubleAnimation();
+            transAnimation.Duration = TimeSpan.FromMilliseconds(500);
+            transAnimation.From = 0;
+            transAnimation.To = 800;
+            storyboard.Children.Add(transAnimation);
+            Storyboard.SetTargetProperty(transAnimation, new PropertyPath("RenderTransform.X"));
+            //Storyboard.SetTargetProperty(transAnimation, new PropertyPath("RenderTransform.Y"));
+            Storyboard.SetTarget(transAnimation, this.MainFrame);
+            storyboard.Completed += new EventHandler(storyboard_Completed);
+            storyboard.Begin();
+        }
+
+        void storyboard_Completed(object sender, EventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation growAnimation = new DoubleAnimation();
+            growAnimation.Duration = TimeSpan.FromMilliseconds(500);
+            growAnimation.From = -800;
+            growAnimation.To = 0;
+            storyboard.Children.Add(growAnimation);
+
+            Storyboard.SetTargetProperty(growAnimation, new PropertyPath("RenderTransform.X"));
+            //Storyboard.SetTargetProperty(growAnimation, new PropertyPath("RenderTransform.Y"));
+            Storyboard.SetTarget(growAnimation, this.MainFrame);
+            storyboard.Begin();
         }
     }
 }
